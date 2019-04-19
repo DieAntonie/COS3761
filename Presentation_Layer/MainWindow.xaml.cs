78fe649cc725d6e504b9ca6aaca06f5a69b8e8b0
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-04-19 16:51:13 +0200

    Swiched to WPF Application

---
+++ Presentation_Layer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentation_Layer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SymbolListBox.Items.Add(ProvisionPropositionalSymbol());
+        }
+
+        private PropositionalSymbol ProvisionPropositionalSymbol()
+        {
+            return new PropositionalSymbol(UpdateSymbolListBox);
+        }
+
+        private bool UpdateSymbolListBox(PropositionalSymbol symbol)
+        {
+            if (symbol == null)
+            {
+                throw new ArgumentNullException(nameof(symbol));
+            }
+            else if (symbol.Deleted)
+            {
+                SymbolListBox.Items.Remove(symbol);
+            }
+            else if (!symbol.Editable && SymbolListBox.Items.IndexOf(symbol) == SymbolListBox.Items.Count - 1)
+            {
+                SymbolListBox.Items.Add(ProvisionPropositionalSymbol());
+            }
+            return true;
+        }
+    }
+}
---
+++ Presentation_Layer/PropositionalSymbol.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentation_Layer
+{
+    /// <summary>
+    /// Interaction logic for PropositionalSymbol.xaml
+    /// </summary>
+    public partial class PropositionalSymbol : UserControl
+    {
+        public bool Deleted { get; private set; } = false;
+        public bool Editable { get; private set; } = true;
+        public Func<PropositionalSymbol, bool> OnChange { get; set; }
+
+        public PropositionalSymbol(Func<PropositionalSymbol, bool> OnChange)
+        {
+            this.OnChange = OnChange;
+            InitializeComponent();
+            RefreshState();
+        }
+
+        private void RefreshState()
+        {
+            OnChange(this);
+            SymbolTextBox.IsEnabled = MeaningTextBox.IsEnabled = Editable;
+            RemoveButton.IsEnabled = !Editable;
+        }
+
+        private void AddEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Editable = !Editable;
+            RefreshState();
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Deleted = true;
+            RefreshState();
+        }
+    }
+}
