c1068e2dbc689822df6d2f62545c7b7e2b83ae75
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-04-20 16:18:03 +0200

    Added something like a base class

---
+++ COS3761/Domain/Symbolic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace COS3761.Domain
+{
+    abstract class Symbolic
+    {
+        private static Dictionary<char, bool> GlobalSymbolList = new Dictionary<char, bool>();
+        public abstract char Symbol { get; set; }
+
+        public Symbolic() { }
+    }
+}
