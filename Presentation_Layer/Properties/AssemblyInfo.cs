47e5eb31f68a70d9093608f013106be69366cbda
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-04-20 14:55:47 +0200

    Some restructuring

--- Presentation_Layer/Properties/AssemblyInfo.cs
+++ Presentation_Layer/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Presentation_Layer")]
+[assembly: AssemblyTitle("COS3761")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Presentation_Layer")]
+[assembly: AssemblyProduct("COS3761")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
