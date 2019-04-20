02eb5850d7d4df65e57e6d58e1b42e54c77ba75c
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-04-20 16:18:55 +0200

    Added initial Constant model

---
+++ COS3761/Domain/Constant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COS3761.Domain
+{
+    class Constant : Symbolic
+    {
+        public override char Symbol { get; set; }
+        public string Description { get; private set; }
+
+        public Constant()
+        {
+
+        }
+    }
+}
